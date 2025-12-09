15c0a361ba096a24be28939f77c01c55fb4842c9
AnitaGeovanaBR <anita.geovana.br@gmail.com>
2025-12-09 11:13:28 -0300

    initial commit

---
+++ API_Aula01/Controllers/FrutasController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Aula01.Controllers
+{
+    [ApiController]
+    public class FrutasController : ControllerBase
+    {
+        private static readonly string[] Frutas = new[]
+        {
+            "Uva", "Banana", "Manga", "Cajá", "Pinha", "Limão", "Maçã"
+        };
+
+        private readonly ILogger<FrutasController> _logger;
+
+        public FrutasController(ILogger<FrutasController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("frutas/todas")]
+        public IEnumerable<string> GetTodas()
+        {
+            return Frutas;
+        }
+    }
+}
